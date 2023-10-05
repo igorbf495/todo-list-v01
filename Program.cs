be1d73858fd1cb9d4e939c2e0547a342e79771bb
igorbf495 <igorcarvalhofelipe495@gmail.com>
2023-10-05 13:43:06 -0300

    feat: estrutura de dados

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Task
+{
+    public int Id { get; set; }        // Identificador único da tarefa
+    public string Title { get; set; }  // Título da tarefa
+    public string Description { get; set; } // Descrição da tarefa
+    public DateTime DueDate { get; set; }  // Data de vencimento da tarefa
+    public bool IsCompleted { get; set; }  // Indica se a tarefa está concluída
+
+    public Task(int id, string title, string description, DateTime dueDate, bool isCompleted)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        DueDate = dueDate;
+        IsCompleted = isCompleted;
+    }
+}
